33b1245ff335bcb203bd3fa9185aafd1c398cae5
Gabriel <gabriel80546@gmail.com>
2023-02-24 19:17:31 -0300

    Daora

--- compile.sh
+++ compile.sh
@@ -1,1 +1,5 @@
+#!/usr/bin/env bash
+echo "rodando: \"gcc main.c -o main -lssl -lcrypto\""
 gcc main.c -o main -lssl -lcrypto
+echo "rodando: echo \$?"
+echo " => $?"
---
+++ test.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+test=teste4
+echo "testando \"$test\""
+echo -n "$test" | openssl sha256
